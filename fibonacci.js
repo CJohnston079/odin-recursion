ba0d65711ab9b70c1f9b9c73d96342e4562c8e5d
Chris <chris79gp@outlook.com>
2024-03-07 14:00:16 +0000

    feat: add fibonacci()

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function () {
-	return [];
+const fibonacci = function (n) {
+	if (n === 0) {
+		return [];
+	}
+
+	if (n === 1) {
+		return [0];
+	}
+
+	const fib = [0, 1];
+
+	for (let i = 2; i < n; i += 1) {
+		const nextNum = fib[fib.length - 1] + fib[fib.length - 2];
+
+		fib.push(nextNum);
+	}
+
+	return fib;
 };
 
 export default fibonacci;
