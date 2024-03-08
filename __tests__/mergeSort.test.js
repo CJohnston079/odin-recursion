9c930b9b1d53ea35e0db8cbb72ba68b1bfafa5e8
Chris <chris79gp@outlook.com>
2024-03-08 11:41:54 +0000

    feat: add mergeSort() and helper merge() mergeSort() takes an array and returns the array sorted in ascending order merge takes two arrays and returns a combined single array sorted in ascending order

--- __tests__/mergeSort.test.js
+++ __tests__/mergeSort.test.js
@@ -10,14 +10,14 @@ describe("mergeSort", () => {
 	it("should correctly sort an array with two numbers in ascending order", () => {
 		expect(mergeSort([4, 1])).toEqual([1, 4]);
 	});
-	it.skip("should correctly sort an array with an odd number of elements in ascending order", () => {
+	it("should correctly sort an array with an odd number of elements in ascending order", () => {
 		expect(mergeSort([4, 1, 6])).toEqual([1, 4, 6]);
 	});
-	it.skip("should correctly sort an array with numbers in ascending order", () => {
+	it("should correctly sort an array with numbers in ascending order", () => {
 		expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
 		expect(mergeSort([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
 	});
-	it.skip("should correctly sort an array with negative numbers in ascending order", () => {
+	it("should correctly sort an array with negative numbers in ascending order", () => {
 		expect(mergeSort([-3, -2, -5, -1, -8])).toEqual([-8, -5, -3, -2, -1]);
 	});
 });
--- mergeSort.js
+++ mergeSort.js
@@ -2,6 +2,29 @@ const mergeSort = function (arr) {
 	if (arr.length <= 1) {
 		return arr;
 	}
+
+	const leftHalf = arr.slice(0, Math.floor(arr.length / 2));
+	const rightHalf = arr.slice(Math.floor(arr.length / 2));
+
+	return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+};
+
+const merge = function (left, right) {
+	let result = [];
+	let leftIndex = 0;
+	let rightIndex = 0;
+
+	while (leftIndex < left.length && rightIndex < right.length) {
+		if (left[leftIndex] < right[rightIndex]) {
+			result.push(left[leftIndex]);
+			leftIndex += 1;
+		} else {
+			result.push(right[rightIndex]);
+			rightIndex += 1;
+		}
+	}
+
+	return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
 };
 
 export default mergeSort;
