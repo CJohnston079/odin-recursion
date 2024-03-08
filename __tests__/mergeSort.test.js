ab29c9139a99edc9b8ca137dc4782d227a54f496
Chris <chris79gp@outlook.com>
2024-03-08 11:14:03 +0000

    test: add unit tests for mergeSort()

--- __tests__/mergeSort.test.js
+++ __tests__/mergeSort.test.js
@@ -1,3 +1,17 @@
 import mergeSort from "../mergeSort";
 
-describe("mergeSort", () => {});
+describe("mergeSort", () => {
+	it("should return an empty array if the input is an empty array", () => {
+		expect(mergeSort([])).toEqual([]);
+	});
+	it("should return the same array if it has only one element", () => {
+		expect(mergeSort([42])).toEqual([42]);
+	});
+	it("should correctly sort an array with numbers in ascending order", () => {
+		expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+		expect(mergeSort([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
+	});
+	it("should correctly sort an array with negative numbers in ascending order", () => {
+		expect(mergeSort([-3, -2, -5, -1, -8])).toEqual([-8, -5, -3, -2, -1]);
+	});
+});
